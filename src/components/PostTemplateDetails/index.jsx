e3981f4770751ce4fba4ac1ee7a757a7752dda4b
Fred Montet <frederic.montet@vast.ch>
2019-03-15 09:34:32 +0100

    Removed unused code

--- src/components/PostTemplateDetails/index.jsx
+++ src/components/PostTemplateDetails/index.jsx
@@ -32,15 +32,6 @@ class PostTemplateDetails extends React.Component {
       </div>
     )
 
-    const commentsBlock = (
-      <div>
-        <Disqus
-          postNode={post}
-          siteMetadata={this.props.data.site.siteMetadata}
-        />
-      </div>
-    )
-
     return (
       <div>
         {homeBlock}
@@ -60,18 +51,6 @@ class PostTemplateDetails extends React.Component {
           </div>
           <div className="post-single__footer">
             {tagsBlock}
-            <hr />
-            <p className="post-single__footer-text">
-              {subtitle}
-              <a
-                href={`https://twitter.com/${author.twitter}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <br /> <strong>{author.name}</strong> on Twitter
-              </a>
-            </p>
-            {commentsBlock}
           </div>
         </div>
       </div>
