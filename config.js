dcd2beb0544165811458e4abf2cd8b25fb004138
Fred Montet <fredmontet@gmail.com>
2020-04-04 10:06:12 +0200

    lumen update

---
+++ config.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  url: 'https://fredmontet.com',
+  pathPrefix: '/',
+  title: 'Fred Montet',
+  subtitle: 'Ideas and Computers',
+  copyright: '© All rights reserved.',
+  disqusShortname: 'fredmontet-blog',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: true,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About',
+      path: '/pages/about'
+    }
+  ],
+  author: {
+    name: 'Frédéric Montet',
+    photo: '/photo.jpg',
+    bio: 'Ideas and Computers',
+    contacts: {
+      email: '',
+      facebook: '',
+      telegram: '',
+      twitter: 'fredmontet',
+      github: 'fredmontet',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'fredmontet',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
